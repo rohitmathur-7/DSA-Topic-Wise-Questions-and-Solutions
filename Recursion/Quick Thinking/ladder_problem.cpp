c6567cab083ef2a0168b9cd146a871e2b3138e36
Rohit Mathur <rohitmathur.mathur.6@gmail.com>
2021-03-04 19:53:25 +0530

    Ladder Problem added!

---
+++ Recursion/Quick Thinking/ladder_problem.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int ladder_problem(int n){
+    //base case
+    if(n<0) return 0; 
+    if(n==0) return 1; //n==0 means how many ways are there to stand on the ground, so obviously there is only 1 way.
+    //rec case
+    //we can take the first step as 1, then the remaining n-1 steps can be walked in f(n-1) ways, same for 2 and 3.
+    return (ladder_problem(n-1)+ladder_problem(n-2)+ladder_problem(n-3));
+}
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<ladder_problem(n)<<endl;
+} 
